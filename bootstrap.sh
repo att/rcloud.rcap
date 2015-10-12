d3d50fc9a9bc8bbdd51f21b422fc28793f83db6e
Douglas Ashton <douglas.j.ashton@gmail.com>
2015-10-12 11:58:32 +0100

    Update bootstrap script to include RStudio server

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,18 +5,31 @@
   echo 'deb http://cran.rstudio.com/bin/linux/ubuntu trusty/' >> /etc/apt/sources.list
   apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E084DAB9
 
+  # Set some user permissions
+  addgroup vagrant staff
+
   apt-get update
   apt-get -y install r-base-dev
 
+
 # RCloud dependencies
   ## Ubuntu 14.04
-  apt-get install -y -qq gcc g++ gfortran libcairo-dev libreadline-dev libxt-dev libjpeg-dev libicu-dev libssl-dev libcurl4-openssl-dev subversion git automake make libtool libtiff-dev gettext redis-server rsync
+  apt-get install -y -qq gcc g++ gfortran libcairo-dev libreadline-dev libxt-dev libjpeg-dev \
+  libicu-dev libssl-dev libcurl4-openssl-dev subversion git automake make libtool \
+  libtiff-dev gettext redis-server rsync
 
-  # Install pandoc 
-  wget -q https://github.com/jgm/pandoc/releases/download/1.13.2/pandoc-1.13.2-1-amd64.deb
-  apt-get -y -q install gdebi-core
-  gdebi -n pandoc-1.13.2-1-amd64.deb
-  rm pandoc-1.13.2-1-amd64.deb
+  # Install pandoc (or RStudio Server)
+  #wget -q https://github.com/jgm/pandoc/releases/download/1.13.2/pandoc-1.13.2-1-amd64.deb
+  #apt-get -y -q install gdebi-core
+  #gdebi -n pandoc-1.13.2-1-amd64.deb
+  #rm pandoc-1.13.2-1-amd64.deb
+
+# Install RStudio Server (for dev)
+fname="rstudio-server-0.99.485-amd64.deb"
+apt-get -y -q install gdebi-core wget
+wget -q http://download2.rstudio.org/$fname
+gdebi --n $fname
+rm $fname
 
 # Setup the directory and give it to the vagrant user
 mkdir /data
@@ -25,7 +38,7 @@ chown vagrant /data
 # Could switch over to bootstrapUser.sh where things won't be run as super user
 
 
-# Installing R packages
+# Set CRAN mirror for installing R packages
   echo "options(repos=structure(c(CRAN=\"http://cran.rstudio.com/\")))" >> /etc/R/Rprofile.site
 
 # Unzip, clone, or whatever, your rcloud source. From the rcloud directory:
@@ -45,4 +58,4 @@ chown vagrant /data
   sh scripts/bootstrapR.sh
 
   # Bring RCloud up
-  scripts/fresh_start.sh
+  # scripts/fresh_start.sh
