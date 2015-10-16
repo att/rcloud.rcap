e2044c1cf94a85a065490c1c5fe47325eec6138a
Douglas Ashton <douglas.j.ashton@gmail.com>
2015-10-16 14:36:54 +0100

    moved processControl methods into one file

--- app/R/processControl.R
+++ app/R/processControl.R
@@ -29,3 +29,53 @@ processControl <- function(rcapControl) {
 processControl.default <- function(rcapControl) {
   return(list())
 }
+
+
+# rplot -------------------------------------------------------------------
+
+
+#' @rdname processControl
+#' @method processControl rplot
+#' 
+processControl.rplot <- function(rcapControl) {
+  
+  # The OCAP function name is the control id
+  funName <- rcapControl$id
+  
+  # Pass the contents of the control to the template
+  funText <- fillTemplate("rplotTemplate.R",
+                          list(control.id=rcapControl$id,
+                               control.controlProperties.1=rcapControl$controlProperties[[1]]$value))
+  
+  sourceControlCode(funText, rcapControl$id)
+  
+  return(funName)
+  
+}
+
+
+# interactive plot --------------------------------------------------------
+
+
+#' @rdname processControl
+#' @method processControl interactiveplot
+processControl.interactiveplot <- function(rcapControl) {
+  
+  # The OCAP function name is the control id
+  funName <- rcapControl$id
+  
+  # Pass the contents of the control to the template
+  funText <- fillTemplate("interactiveplotTemplate.R",
+                          list(control.id=rcapControl$id,
+                               control.controlProperties.1=rcapControl$controlProperties[[1]]$value))
+  
+  sourceControlCode(funText, rcapControl$id)
+  
+  return(funName)
+  
+}
+
+
+
+
+
