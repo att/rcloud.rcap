8dcc01dbcd05eccb80342f45e547f90b3a43a0f4
Douglas Ashton <douglas.j.ashton@gmail.com>
2015-10-16 17:46:39 +0100

    adding codetools reminder

--- app/inst/testData/controlClasses.R
+++ app/inst/testData/controlClasses.R
@@ -7,10 +7,18 @@ Control <- R6Class("Control",
                   public = list(
                     id = NA,     # eg rcapac34a must match the update function name
                     type = NA,   # rplot etc
+                    
+                    # Many controls are associated with a data object
                     object=NULL, # The name of a variable in the global environment
                     
+                    # Many controls are associated with a function
+                    controlFunction,
+                    
+                    # Out an in-coming edges from the dependency graph
                     downStream = list(),
                     upStream = list(),
+                    
+                    #### METHODS ####
                     
                     initialize = function(id, type) {
                       if (!missing(id)) self$id <- id
@@ -25,9 +33,14 @@ Control <- R6Class("Control",
                   )
 )
 
+# Could create the objects from a list of names
 controlIDs <- c("rcap1", "rcap2", "rcap3")
-
-
-
 controls <- lapply(controlIDs, Control$new, "rplot")
 controls
+
+# Or we could parse straight from the JSON
+
+
+# Remember this
+library(codetools)
+findGlobals(abPlot)
