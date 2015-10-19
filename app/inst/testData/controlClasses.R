579cce5a56ef4c66683bb22dfa7ba425271612e1
Douglas Ashton <douglas.j.ashton@gmail.com>
2015-10-19 12:09:07 +0100

    Half way moving to R6 control objects

--- app/inst/testData/controlClasses.R
+++ app/inst/testData/controlClasses.R
@@ -1,18 +1,22 @@
 library(R6)
 library(jsonlite)
 
-rcapConfig <- 
+rcapConfig <- jsonlite::fromJSON(system.file("testData/testConfig.json", package="rcloud.rcap"), simplifyVector = FALSE)
 
-Control <- R6Class("Control",
+allControls <- getControls(rcapConfig)
+
+Control <- R6::R6Class("Control",
                   public = list(
-                    id = NA,     # eg rcapac34a must match the update function name
+                    id = NA,     # eg rcapac34a
                     type = NA,   # rplot etc
+                    json = NA,   # The list as returned from JSON
                     
                     # Many controls are associated with a data object
                     object=NULL, # The name of a variable in the global environment
                     
                     # Many controls are associated with a function
-                    controlFunction,
+                    controlFunction=NULL,
+                    ocapFunction=NULL,
                     
                     # Out an in-coming edges from the dependency graph
                     downStream = list(),
@@ -20,27 +24,45 @@ Control <- R6Class("Control",
                     
                     #### METHODS ####
                     
-                    initialize = function(id, type) {
-                      if (!missing(id)) self$id <- id
-                      if (!missing(type)) self$type <- type
+                    # Build the object from the input json
+                    # cl is the json for the control converted to a list
+                    initialize = function(cl) {
+                      if (!is.null(cl$id)) self$id <- cl$id
+                      if (!is.null(cl$type)) self$type <- cl$type
+                      
+                      # Grab the code if it's there
+                      if(!is.null(cl$controlProperties)) {
+                        if(length(cl$controlProperties)>0) {
+                          if(!is.null(cl$controlProperties[[1]]$value)) {
+                            self$controlFunction <- cl$controlProperties[[1]]$value
+                          }
+                        }
+                      }
+                      
+                      # Copy the input list for future reference
+                      self$json <- cl
+                      attr(self$json, "class") <- self$type
+                      
+                      self$ocapFunction <- processControl(self$json)
+                      
                     },
                     
+                    # Call the ocap
                     update = function() {
-                      if(is.function(get(self$id))) {
-                        do.call(self$id, list())
+                      if(is.function(get(self$ocapFunction))) {
+                        do.call(self$ocapFunction, list()) 
                       }
                     }
+                    
                   )
 )
 
 # Could create the objects from a list of names
-controlIDs <- c("rcap1", "rcap2", "rcap3")
-controls <- lapply(controlIDs, Control$new, "rplot")
-controls
+controls <- lapply(allControls, Control$new)
+# Use the ids to name the list items
+names(controls) <- sapply(controls, function(x) x$id)
 
-# Or we could parse straight from the JSON
 
 
 # Remember this
 library(codetools)
-findGlobals(abPlot)
