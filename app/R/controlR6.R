3e7c96e7d2e65853dec9b673656e0cd3609f71f9
Gabor Csardi <csardi.gabor@gmail.com>
2015-10-29 11:58:42 +0000

    Clean up controller and control code
    No real changes, though.

--- app/R/controlR6.R
+++ app/R/controlR6.R
@@ -1,98 +1,106 @@
-# R6 class for controls
-
-#' @importFrom codetools findGlobals
 
 Control <- R6::R6Class("Control",
-           public = list(
-             id = NA,     # eg rcapac34a
-             type = NA,   # rplot etc
-             json = NA,   # The list as returned from JSON
-             
-             # Control Properties
-             controlFunction=NA,
-             variableName=NA,
-             ocapFunction=NULL,
-             
-             # Out an in-coming edges from the dependency graph
-             downStream = list(),
-             upStream = list(),
-             
-             #### METHODS ####
-             
-             # Build the object from the input json
-             # cl is the json for the control converted to a list
-             initialize = function(cl) {
-               if (!is.null(cl$id)) self$id <- cl$id
-               if (!is.null(cl$type)) self$type <- cl$type
-               
-               # Grab the code if it's there
-               if(!is.null(cl$controlProperties)) {
-                 if(length(cl$controlProperties)>0) {
-                   
-                   # Pulling various properties
-                   for (cp in cl$controlProperties) {
-                     
-                     if(cp$uid == "code") {
-                       self$controlFunction <- cp$value
-                     }
-                     if(cp$uid == "variablename") {
-                       self$variableName <- cp$value
-                     }
-                     
-                   }
-                   
-                 }
-               }
-               
-               # Copy the input list for future reference
-               self$json <- cl
-               if(!is.na(self$type)) {
-                 attr(self$json, "class") <- self$type
-               }
-               
-               self$ocapFunction <- processControl(self$json)
-               
-             },
-             
-             # Call the ocap
-             # Fix the environment
-             update = function() {
-               if(is.function(get(self$ocapFunction))) {
-                 do.call(self$ocapFunction, list()) 
-               }
-             },
-             
-             # Compare the variables that are client side to those used in the function
-             # Return the matches
-             dependentVariables = function(clientVars) {
-               
-               varsUsed <- ""
-               
-               if(!is.null(self$controlFunction) & !is.na(self$controlFunction)) {
-                 if(exists(self$controlFunction)) {
-                   if(is.function(get(self$controlFunction))) {
-                     varsUsed <- codetools::findGlobals(get(self$controlFunction))
-                   }
-                 }
-               }
-               
-               # Only choose from client variables
-               clientVars[clientVars %in% varsUsed]
-             },
-             
-             # Add a control that should update when this one does
-             addDownStream = function(downControl) {
-               self$downStream <- append(self$downStream, downControl)
-             },
-             
-             # Add a control that causes this one to update.
-             # Also add the reverse dependency
-             addUpStream = function(upControl) {
-               self$upStream <- append(self$upStream, upControl)
-               upControl$addDownStream(self)
-             }
-             
-             
-             
-          )
+
+  public = list(
+
+    ## Build the object from the input json
+    ## cl is the json for the control converted to a list
+    initialize = function(cl)
+      controlInitialize(self, private, cl),
+
+    getId = function() private$id %||% NA_character_,
+
+    update = function()
+      controlUpdate(self, private),
+
+    dependentVariables = function(clientVars)
+      controlDependentVariables(self, private, clientVars),
+
+    addDownStream = function(downControl)
+      controlAddDownStream(self, private, downControl),
+
+    addUpstream = function(upControl)
+      controlAddUpstream(self, private, upcontrol)
+  ),
+
+  private = list(
+
+    id = NULL,     # eg rcapac34a
+    type = NULL,   # rplot etc
+    json = NULL,   # The list as returned from JSON
+
+    ## Control Properties
+    controlFunction = NULL,
+    variableName = NULL,
+    ocapFunction = NULL,
+
+    ## Out an in-coming edges from the dependency graph
+    downStream = list(),
+    upStream = list()
+  )
 )
+
+controlInitialize <- function(self, private, cl) {
+
+  if (!is.null(cl$id)) private$id <- cl$id
+  if (!is.null(cl$type)) private$type <- cl$type
+
+  ## Grab the code if it's there
+  if (!is.null(cl$controlProperties)) {
+    if (length(cl$controlProperties) > 0) {
+      ## Pulling various properties
+      for (cp in cl$controlProperties) {
+        if (cp$uid == "code") private$controlFunction <- cp$value
+        if (cp$uid == "variablename") private$variableName <- cp$value
+      }
+    }
+  }
+
+  ## Copy the input list for future reference
+  private$json <- cl
+  if (!is.null(private$type)) attr(private$json, "class") <- private$type
+
+  private$ocapFunction <- processControl(private$json)
+
+  invisible(self)
+}
+
+controlUpdate <- function(self, private) {
+  ## Call the ocap
+  ## Fix the environment
+  if (is.function(get(private$ocapFunction))) {
+    do.call(private$ocapFunction, list())
+  }
+}
+
+## Compare the variables that are client side to those used in the function
+## Return the matches
+#' @importFrom codetools findGlobals
+
+dependentVariables = function(clientVars) {
+
+  varsUsed <- character(0)
+
+  if (!is.null(private$controlFunction) &&
+       exists(private$controlFunction) &&
+       is.function(get(private$controlFunction))) {
+    varsUsed <- codetools::findGlobals(get(private$controlFunction))
+  }
+
+  ## Only choose from client variables
+  clientVars[clientVars %in% varsUsed]
+}
+
+
+## Add a control that should update when this one does
+contolAddDownStream <- function(self, private, downControl) {
+  private$downStream <- append(private$downStream, downControl)
+  invisible(self)
+}
+
+## Add a control that causes this one to update
+controlAddUpStream <- function(self, private, upControl) {
+  private$upStream <- append(private$upStream, upControl)
+  upControl$addDownStream(self)
+  invisible(self)
+}
