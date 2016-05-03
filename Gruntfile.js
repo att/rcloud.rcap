97cd7af6f0005b812a9af313e67218550f060d38
shane <shane@freshclickmedia.com>
2016-05-03 15:27:32 +0100

    version converter

--- Gruntfile.js
+++ Gruntfile.js
@@ -40,9 +40,7 @@ module.exports = function(grunt) {
 
                 },
                 files: {
-                    'app/inst/www/styles/default.css': 'app/inst/www/styles/default.scss',
-                    'app/inst/www/themes/bragi/bragi.css': 'app/inst/www/themes/bragi/bragi.scss',
-                    'app/inst/www/themes/att/att.css': 'app/inst/www/themes/att/att.scss',
+                    'app/inst/www/styles/default.css': 'app/inst/www/styles/default.scss'
                 },
                 trace: true
             }
