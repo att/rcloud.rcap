36b9e8df42231e5c840751ac32617e749e2c6fed
Gábor Csárdi <csardi.gabor@gmail.com>
2016-08-18 11:02:48 +0100

    Install script shows grunt output now

--- install.sh
+++ install.sh
@@ -1,7 +1,17 @@
 #! /usr/bin/env bash
 
+set -e
+
 echo "WARNING: Not running 'npm install', you might need to run it manually"
 echo "WARNING: Not running 'bower install', you might need to run it manually"
+
+# R CMD build ignores the output and the exit value of the cleanup
+# script, so we run grunt manually here
+
+(
+    cd inst
+    grunt
+)
 
 (
     pkgdir=$(basename `pwd`)
