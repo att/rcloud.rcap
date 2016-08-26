9114a04fd1b1167993962606249b0af1752e49b6
Gábor Csárdi <csardi.gabor@gmail.com>
2016-08-26 09:14:13 +0000

    ./install --full to make a full build, i.e. run npm & bower as well
    You can also run it to create a release.

--- install.sh
+++ install.sh
@@ -2,15 +2,52 @@
 
 set -e
 
-echo "WARNING: Not running 'npm install', you might need to run it manually"
-echo "WARNING: Not running 'bower install', you might need to run it manually"
+# Defaults
+full=false
+
+usage() {
+    echo "Usage: $0 [-h|--help] [-f|--full]"
+}
+
+while [[ $# -gt 0 ]]; do
+    key="$1"
+
+    case $key in
+        -h|--help)
+	    usage
+	    exit 2
+	    ;;
+	-f|--full)
+	    full=true
+	    ;;
+	*)
+	    echo Unknown option: $key
+	    usage
+	    exit 1
+	    ;;
+    esac
+    shift
+done
+
+if [[ $full != "true" ]]; then
+    echo "WARNING: Skipping 'npm install', you may need to run it manually"
+    echo "WARNING: Skipping 'bower install', you may need to run it manually"
+fi
 
 # R CMD build ignores the output and the exit value of the cleanup
 # script, so we run grunt manually here
+# If --full is specified, then we run npm install & bower install as well,
+# via the cleanup script
+
+export full
 
 (
-    cd inst
-    grunt
+    if [[ $full == "true" ]]; then
+	. cleanup
+    else
+	cd inst
+	grunt
+    fi
 )
 
 (
