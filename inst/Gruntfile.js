aca06e8216a30c86b71f9d57b83f400ce4180661
Shane Porter <shane@freshclickmedia.com>
2016-09-15 21:16:52 +0100

    fixes #143

--- inst/Gruntfile.js
+++ inst/Gruntfile.js
@@ -106,7 +106,7 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-shell');
     grunt.loadNpmTasks('grunt-open');
     grunt.loadNpmTasks('grunt-contrib-clean');
-    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-sass');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-newer');
