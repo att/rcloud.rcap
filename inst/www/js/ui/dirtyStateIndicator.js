5a1e91db8c8233fa047851f016c56d072f8c0230
Shane Porter <shane@freshclickmedia.com>
2016-11-07 16:31:02 +0000

    fixes #175

--- inst/www/js/ui/dirtyStateIndicator.js
+++ inst/www/js/ui/dirtyStateIndicator.js
@@ -27,17 +27,18 @@ define(['pubsub', 'site/pubSubTable', 'rcap/js/utils/rcapLogger'], function(PubS
             PubSub.subscribe(pubSubTable.gridInitComplete, function() {
                 // these events incur a modified state:
                 var modifyingEvents = [
-                   pubSubTable.pageAdded,
-                   pubSubTable.deletePageConfirm,
-                   pubSubTable.duplicatePageConfirm,
-                   pubSubTable.dataSourceAdded,
-                   pubSubTable.deleteDataSourceConfirm,
-                   pubSubTable.updateDataSource,
-                   pubSubTable.timerAdded,
-                   pubSubTable.deleteTimerConfirm,
-                   pubSubTable.updateTimer,
-                   pubSubTable.updateControl,
-                   pubSubTable.gridItemsChanged
+                    pubSubTable.updateSiteSettings,
+                    pubSubTable.pageAdded,
+                    pubSubTable.deletePageConfirm,
+                    pubSubTable.duplicatePageConfirm,
+                    pubSubTable.dataSourceAdded,
+                    pubSubTable.deleteDataSourceConfirm,
+                    pubSubTable.updateDataSource,
+                    pubSubTable.timerAdded,
+                    pubSubTable.deleteTimerConfirm,
+                    pubSubTable.updateTimer,
+                    pubSubTable.updateControl,
+                    pubSubTable.gridItemsChanged
                     // updating themes does not invoke the dirty flag because
                     // they are saved when the save button on the theme editor
                     // dialog is clicked
@@ -46,7 +47,7 @@ define(['pubsub', 'site/pubSubTable', 'rcap/js/utils/rcapLogger'], function(PubS
                 modifyingEvents.forEach(function(e) {
                     PubSub.subscribe(e, function(msg, msgInfo) {
                         // if dirty has been set AND is true:
-                        if(!_.isUndefined(msgInfo.isDirty) && msgInfo.isDirty) {
+                        if(_.isUndefined(msgInfo) || (!_.isUndefined(msgInfo.isDirty) && msgInfo.isDirty) || _.isUndefined(msgInfo.isDirty)) {
                             rcapLogger.info('dirtyStateIndicator, setting modified after receiving: pubSubTable.' + msg);
                             isDirty = true;
                             el.show();  
