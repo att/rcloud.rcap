fa5b9f6de8b7ce9a41afbf77d9eab0098bd795e7
Shane Porter <shane@freshclickmedia.com>
2017-11-06 13:19:46 +0000

    less aggressive key filtering in execution order inputs

--- inst/www/js/ui/dialogManager.js
+++ inst/www/js/ui/dialogManager.js
@@ -627,9 +627,21 @@ define([
                 }));
 
                 $('#dialog-executionOrderSettings').find('tbody').html(tableBody);
-                $('#dialog-executionOrderSettings').find('input').on('keypress', function(e) {
-                    if(!(e.charCode >= 48 && e.charCode <= 57)) {
-                        e.preventDefault();
+                $('#dialog-executionOrderSettings').find('input').keypress(function(event) {
+                    // Backspace, tab, enter, end, home, left, right
+                    // We don't support the del key in Opera because del == . == 46.
+                    var controlKeys = [8, 9, 13, 35, 36, 37, 39];
+                    // IE doesn't support indexOf
+                    var isControlKey = controlKeys.join(',').match(new RegExp(event.which));
+                    // Some browsers just don't raise events for control keys. Easy.
+                    // e.g. Safari backspace.
+                    if (!event.which || // Control keys in most browsers. e.g. Firefox tab is 0
+                        (49 <= event.which && event.which <= 57) || // Always 1 through 9
+                        (48 === event.which && $(this).attr('value')) || // No 0 first digit
+                        isControlKey) { // Opera assigns values for control keys.
+                        return;
+                    } else {
+                        event.preventDefault();
                     }
                 });
 
