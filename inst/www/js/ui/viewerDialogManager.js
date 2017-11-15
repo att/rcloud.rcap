b5a701ff90a8c5f0f6be1c66a95aa5c28d154c2b
Shane Porter <shane@freshclickmedia.com>
2017-11-15 15:47:32 +0000

    Small grammar change
    0 Byte -> 0 Bytes

--- inst/www/js/ui/viewerDialogManager.js
+++ inst/www/js/ui/viewerDialogManager.js
@@ -280,7 +280,7 @@ define([
         function bytesToSize(bytes) {
            var sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB'];
            if (bytes === 0) {
-             return '0 Byte';
+             return '0 Bytes';
            }
            var i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)));
            return Math.round(bytes / Math.pow(1024, i), 2) + ' ' + sizes[i];
