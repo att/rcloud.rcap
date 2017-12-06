b06ee6547caa01a30b2fe5b1600f31cb80ba553d
Mateusz Rogalski <useless5771@gmail.com>
2017-12-06 14:08:27 +0000

    FIX #286 add rcap.messageWidget.msg and rcap.progressSpinner.msg functions

--- R/rcap.messageWidget.R
+++ R/rcap.messageWidget.R
@@ -26,3 +26,26 @@ rcap.messageWidget.write <- function(variableName, message, append=FALSE) {
   }
   invisible(NULL)
 }
+
+#' @title Write message to a collection of Message widgets
+#' 
+#' @param msg named list of messages, with keys being Message widget variableNames
+#' @param append flag specifying if the messages should be appended to widgets' content or not
+#' @param ... named parameters, if 'msg' is not defined, these specify progress spinners and messages that should be written.
+#' @return NULL invisibly
+#' @export
+rcap.messageWidget.msg <- function(msg=NULL, append=FALSE, ...) {
+  parms <- list(...)
+  if(is.null(msg) && length(parms) >0 ) {
+    msg <- parms
+  }
+  if( length(msg) > 0 ) {
+    msg <- as.list(msg)
+    for(p in 1:length(msg)) {
+      if(!is.na(names(msg)[[p]])) {
+        rcloud.rcap:::rcap.messageWidget.write(names(msg)[[p]], msg[[p]], append = append)
+      }
+    }
+  }
+  invisible(NULL)
+}
